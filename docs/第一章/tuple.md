61ca40c6c53c0e0d1756cca8896a45f03f590788
gyileng <879663394@qq.com>
2021-04-12 15:31:53 +0800

    折叠导航栏

---
+++ docs/第一章/tuple.md
@@ -0,0 +1,15 @@
+# 数组
+
+### 定义  
+数组（Array）是有序的元素序列。若将有限个类型相同的变量的集合命名，那么这个名称为数组名。组成数组的各个变量称为数组的分量，也称为数组的元素，
+有时也称为下标变量。用于区分数组的各个元素的数字编号称为下标。数组是在程序设计中，为了处理方便， 把具有相同类型的若干元素按有序的形式组织起来
+的一种形式。这些有序排列的同类数据元素的集合称为数组。
+
+### 小试牛刀  
+| No. | Title | Solution | Difficulty | TimeComplexity | SpaceComplexity | Favorite | Acceptance | Download |
+| :------------: | :------------: | :------------: | :------------: | :------------: | :------------: | :------------: | :------------: | :------------: |
+| 26 | 删除排序数组中的重复项 | [Go](/problems/26_remove_duplicates/readme.md) | <span class="level-easy">简单</span> | O(n) | O(1) | :heart: | 53.2% | [:arrow_down:](/problems/26_remove_duplicates/leetcode.py) |
+| 122 | 买卖股票的最佳时机 II | [Go](/problems/122_max_profit/readme.md) | <span class="level-easy">简单</span> | O(n) | O(1) |   | 67.2% | [:arrow_down:](/problems/122_max_profit/leetcode.py) |
+| 189 | 旋转数组 | [Go](/problems/189_rotate/rotate.md) | <span class="level-hard">中等</span> | O(n) | O(1) |   | 45.9% | [:arrow_down:](/problems/189_rotate/rotate.py) |
+| 217 | 存在重复元素 | [Go](/problems/217_contains_duplicate/contains_duplicate.md) | <span class="level-easy">简单</span> | O(1) | O(1) |   | 55.5% | [:arrow_down:](/problems/217_contains_duplicate/contains_duplicate.py) |
+| 136 | 只出现一次的数字 | [Go](/problems/136_single_number/single_number.md) | <span class="level-easy">简单</span> | O(n) | O(1) |   | 71.2% | [:arrow_down:](/problems/136_single_number/single_number.py) |
--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@ $(document).on("click", "#return-top-bottom>div", function () {
     $('html,body').animate({scrollTop: scroll_val}, 800)
 });
 
-// // nav收缩展开
+// nav收缩展开
 // $(document).on("click", ".sidebar-nav>ul>li>strong", function () {
 //     if ($(this).next().css('display') == "none") {
 //         //展开未展开
@@ -16,17 +16,17 @@ $(document).on("click", "#return-top-bottom>div", function () {
 //         $(this).next('ul').slideUp(300);
 //         $('.nav-item.nav-show').removeClass('nav-show');
 //     }
-// })
-//
-// $(document).on("click", ".sidebar-nav>ul>li>ul>li>strong", function () {
-//     if ($(this).next().css('display') == "none") {
-//         //展开未展开
-//         $(this).children('ul').slideUp(300);
-//         $(this).next('ul').slideDown(300);
-//         $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
-//     } else {
-//         //收缩已展开
-//         $(this).next('ul').slideUp(300);
-//         $('.nav-mini.nav-show').removeClass('nav-show');
-//     }
 // });
+
+$(document).on("click", ".sidebar-nav>ul>li>ul>li>a", function () {
+    if ($(this).next().css('display') == "none") {
+        //展开未展开
+        $(this).children('ul').slideUp(300);
+        $(this).next('ul').slideDown(300);
+        $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
+    } else {
+        //收缩已展开
+        $(this).next('ul').slideUp(300);
+        $('.nav-mini.nav-show').removeClass('nav-show');
+    }
+});
